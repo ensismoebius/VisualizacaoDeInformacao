2e723cea9b07ddbc8369c50dfe12f85c2ff42af5
Andre Furlan <ensismoebius@gmail.com>
2023-07-14 21:11:03 -0300

    System is ready!!!

--- dashboard2/main.py
+++ dashboard2/main.py
@@ -7,33 +7,63 @@ import plotly.graph_objects as go
 
 from data import Dashdata
 
+
 class Dashboard():
     def __init__(self):
+
+        #######################
+        ### Core properties ###
+        #######################
+
         # Data container
         self.ddata = Dashdata()
-        
+
+        self.selectedModality = -1
+
+        self.selectedArtifact = -1
+
+        self.selectedEstimuli = -1
+
+        self.generalView = False
+
+        self.subjects = self.ddata.dataframe
+
         # Create the SVG pane widget
         self.svg_image = pn.pane.SVG(
-            SVG(self.ddata.brainSvgImage), 
-            width=600, 
+            SVG(self.ddata.brainSvgImage),
+            width=600,
             height=600)
-        
+
         ########################
         ### Widgets creation ###
         ########################
-        
+
         # Create the Plotly pane widget
         self.plot_pane = pn.pane.Plotly()
-        
-        # Create the slider widget
-        self.intRangeSliderWidget = pn.widgets.IntSlider(name='Integer Range Slider', start=0, end=10, value=0, step=1)
-        
-        # Create the selection widget
-        self.selectWidget = pn.widgets.Select(name='Selecione a sujeito', groups=self.ddata.subjectsNamesAndCodes)
+
+        # Create the subject selection widget
+        self.selectSubjectWidget = pn.widgets.Select(
+            name='Selecione a sujeito')
+
+        # Create the modality selection widget
+        self.selectModalityWidget = pn.widgets.Select(
+            name='Selecione a modalidade')
+        self.selectModalityWidget.options = self.ddata.modalities
+
+        # Create the artfact selection widget
+        self.selectArtefactWidget = pn.widgets.Select(
+            name='Selecione o artefato')
+        self.selectArtefactWidget.options = self.ddata.artfacts
+
+        # Create the estimul selection widget
+        self.selectStimuliWidget = pn.widgets.Select(
+            name='Selecione o estímulo')
+        self.selectStimuliWidget.options = self.ddata.estimulos
 
         # Create a label for the switch widget
-        self.switchLabelWidget = pn.widgets.StaticText(name='Visão geral', value='')
-        
+        self.switchLabelWidget = pn.widgets.StaticText(
+            name='Visão geral', value='')
+
         # Create the switch widget
         self.switchWidget = pn.widgets.Switch(name='Visão geral')
 
@@ -41,27 +71,125 @@ class Dashboard():
         ### Event handle binding ###
         ############################
 
-        # Bind the widgets to its respective event handler
-        self.intRangeSliderWidget.param.watch(self._update_plot, 'value')
-        self.selectWidget.param.watch(self._selectData, 'value')
-        
+        self.selectModalityWidget.param.watch(self._modalitySelected, 'value')
+        self.selectStimuliWidget.param.watch(self._estimuliSelected, 'value')
+        self.selectArtefactWidget.param.watch(self._artifactSelected, 'value')
+        self.selectSubjectWidget.param.watch(self._update_plot, 'value')
+        self.switchWidget.param.watch(self._generalViewChange, 'value')
+
         #######################
         ### Layout assembly ###
         #######################
-        
+
         self.layout = pn.Row(
-            self.svg_image, 
+            self.svg_image,
             pn.Column(
-                pn.Row(self.intRangeSliderWidget, self.selectWidget),
+                pn.Row(self.selectModalityWidget, self.selectStimuliWidget),
+                pn.Row(self.selectArtefactWidget, self.selectSubjectWidget),
                 self.plot_pane,
                 pn.Row(self.switchLabelWidget, self.switchWidget)
-                ), 
+            ),
             sizing_mode='stretch_width')
 
-    def _selectData(event):
-        selected_option = event.obj.value
-        return selected_option
-        
+    ############################
+    ### Callbacks definition ###
+    ############################
+
+    def _generalViewChange(self, event):
+        self.generalView = event.new
+        self._update_plot(event, self.selectSubjectWidget.value)
+
+    def _modalitySelected(self, event):
+        self.selectedModality = event.new
+        if (self.selectedModality != -1) and (self.selectedEstimuli != -1):
+            self.subjects = self.filterSignals(
+                self.selectedModality, self.selectedEstimuli, self.selectedArtifact)
+            self.selectSubjectWidget.options = self.subjects.index.tolist()
+
+    def _estimuliSelected(self, event):
+        self.selectedEstimuli = event.new
+        if (self.selectedModality != -1) and (self.selectedEstimuli != -1):
+            self.subjects = self.filterSignals(
+                self.selectedModality, self.selectedEstimuli, self.selectedArtifact)
+            self.selectSubjectWidget.options = self.subjects.index.tolist()
+
+    def _artifactSelected(self, event):
+        self.selectedArtifact = event.new
+        if (self.selectedModality != -1) and (self.selectedArtifact != -1):
+            self.subjects = self.filterSignals(
+                self.selectedModality, self.selectedEstimuli, self.selectedArtifact)
+            self.selectSubjectWidget.options = self.subjects.index.tolist()
+
+    def _update_plot(self, event, subjectNumber = -1):
+        lines = []
+        if subjectNumber == -1:
+            subjectNumber = event.new
+
+        # Extract data to be plot
+        for sensor in self.ddata.sensorToColorRelationship:
+            try:
+                # Sensor name + signal for example: (F3, 12121212112)
+                lines.append(
+                    (sensor, self.subjects.loc[[subjectNumber]][sensor].values[0]))
+            except Exception:
+                pass
+
+        fig = go.Figure()
+
+        if self.generalView:
+            
+            #########################
+            ### Single panel plot ###
+            #########################
+            
+            # Iterate over the y data and create traces for each line
+            for i, line in enumerate(lines):
+                # Retrieves the color of the sensor
+                color = self.ddata.sensorToColorRelationship[line[0]]
+                fig.add_trace(go.Scatter(y=line[1], mode='lines', name=line[0], line=dict(color=color)))
+        else:
+            
+            ###########################
+            ### Multiple panel plot ###
+            ###########################
+
+            # Create the subplots
+            fig = sp.make_subplots(rows=len(lines), cols=1)
+
+            # Plots data
+            for i, line in enumerate(lines):
+
+                # Retrieves the color of the sensor
+                color = self.ddata.sensorToColorRelationship[line[0]]
+
+                fig.add_trace(
+                    # Plots the line with the corresponding color and name of the sensor
+                    go.Scatter(y=line[1], mode='lines',
+                               name=line[0], line=dict(color=color)),
+                    row=i+1, col=1)
+
+        # Refresh the plots UI
+        fig.update_layout()
+        self.plot_pane.object = fig
+
+        # Refresh the Svg UI
+        self.updateBrainFigure(subjectNumber, lines)
+
+        return fig
+
+    ##########################
+    ### Methods definition ###
+    ##########################
+
+    def print_subjects_by_group(self):
+        subjects_by_group = self.ddata.extract_subjects_by_group()
+
+        for group, subjects in subjects_by_group.items():
+            modality, artifacts = group
+            print(f"Modalidade: {modality}, Artefatos: {artifacts}")
+            print(subjects)
+            print()
+
     def _changeElementHeight(self, soup, element_id, new_height):
         element = soup.find(id=element_id)
         if element is not None:
@@ -73,64 +201,48 @@ class Dashboard():
         if element is not None:
             color = self.ddata.sensorToColorRelationship[sensorsTags]
             element["style"] = f'fill:{color}'
-            
+
     def calculate_total_energy(self, values):
         squared_values = np.square(values)       # Square each value
         sum_of_squares = np.sum(squared_values)  # Sum up the squared values
         total_energy = np.sqrt(sum_of_squares)   # Take the square root
-    
+
         return total_energy
-            
+
     def updateBrainFigure(self, event, lines):
-        
+
         # Loads the SVG
         soup = BeautifulSoup(self.ddata.brainSvgImage, "xml")
-        
+
         # Iterates over all available sensors
         for i, line in enumerate(lines):
-            
+
             # Change sensor color
             self._paintSensor(soup, line[0])
-            
+
             # Change sensor energy height
-            self._changeElementHeight(soup, line[0] +"bar", 5 * self.calculate_total_energy(line[1]))
-            
+            self._changeElementHeight(
+                soup, line[0] + "bar", 5 * self.calculate_total_energy(line[1]))
+
         # Must be the last call!
         self.svg_image.object = SVG(str(soup))
-        
-    def _update_plot(self, event):
-        lines = []
-        
-        # Extract data to be plot
-        for sensor in self.ddata.sensorToColorRelationship:
-            try:
-                # Sensor name + signal for example: (F3, 12121212112)
-                lines.append((sensor, self.ddata.dataframe.iloc[[event.new]][sensor].values[0]))
-            except Exception:
-                pass
 
-        # Create the nned subplots
-        fig = sp.make_subplots(rows=len(lines), cols=1)
+    def filterSignals(self, modalidade, estimulo, artefato=-1):
 
-        # Plots data
-        for i, line in enumerate(lines):
-            
-            # Retrieves the color of the sensor
-            color = self.ddata.sensorToColorRelationship[line[0]]
-            
-            fig.add_trace(
-                # Plots the line with the corresponding color and name of the sensor 
-                go.Scatter(y=line[1], mode='lines', name=line[0], line=dict(color=color)),
-            row=i+1, col=1)
+        print(f'{modalidade}, {estimulo}, {artefato}')
 
-        # Refresh the plots UI
-        fig.update_layout(height=600, width=800, title='Line Plots')
-        self.plot_pane.object = fig
+        if (artefato != -1):
+            return self.ddata.dataframe[
+                (self.ddata.dataframe.Modalidade == modalidade) &
+                (self.ddata.dataframe.Estímulo == estimulo) &
+                (self.ddata.dataframe.Artefatos == artefato)
+            ]
 
-        # Refresh the Svg UI
-        self.updateBrainFigure(event.new, lines)
-        
-        return fig
+        return self.ddata.dataframe[
+            (self.ddata.dataframe.Modalidade == modalidade) &
+            (self.ddata.dataframe.Estímulo == estimulo)
+        ]
+
 
 dashboard = Dashboard()
 dashboard.layout.servable()
